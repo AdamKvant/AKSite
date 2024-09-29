3bb9f1c42f219eb06f994567a41e8ed66598d2d7
AdamKvant <adamkvant@gmail.com>
2024-09-29 14:44:15 -0500

    got test server working

---
+++ server.py
@@ -0,0 +1,43 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+def server(url):
+    if ("/main" in url) or ("/" in url and len(url) == 1):
+        return open("index.html").read(), "text/html"
+    elif url == "/css/style.css":
+        return open("css/style.css").read() , "text/css"
+    else:
+        return open("404.html").read(), "text/html"
+    
+
+# You shouldn't need to change content below this. It would be best if you just left it alone.
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Call the student-edited server code.
+        message, content_type = server(self.path)
+
+        # Convert the return value into a byte string for network transmission
+        if type(message) == str:
+            message = bytes(message, "utf8")
+
+        # prepare the response object with minimal viable headers.
+        self.protocol_version = "HTTP/1.1"
+        # Send response code
+        self.send_response(200)
+        # Send headers
+        # Note -- this would be binary length, not string length
+        self.send_header("Content-Length", len(message))
+        self.send_header("Content-Type", content_type)
+        self.send_header("X-Content-Type-Options", "nosniff")
+        self.end_headers()
+
+        # Send the file.
+        self.wfile.write(message)
+        return
+
+def run():
+    PORT = 4131
+    print(f"Starting server http://localhost:{PORT}/")
+    server = ('', PORT)
+    httpd = HTTPServer(server, RequestHandler)
+    httpd.serve_forever()
+run()
